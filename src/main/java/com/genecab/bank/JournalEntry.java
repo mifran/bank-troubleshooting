87dd1e98ed327e58dcc1b277f1c6d9fbe155f52c
Mitch Franklin <biz@kingsforum.com>
2023-04-24 05:36:39 -0400

    On JournalEntry repos and spring data lab, page 7, step 4. Next is edit data.sql.

--- src/main/java/com/genecab/bank/JournalEntry.java
+++ src/main/java/com/genecab/bank/JournalEntry.java
@@ -1,5 +1,7 @@
 package com.genecab.bank;
 
-public record JournalEntry(Long id, String description, Long instantAsLong, String amount, String balance) {
+import org.springframework.data.annotation.Id;
+
+public record JournalEntry(@Id Long id, String description, Long instant, String amount, String balance) {
 
 }
--- src/main/java/com/genecab/bank/JournalEntryController.java
+++ src/main/java/com/genecab/bank/JournalEntryController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.Instant;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journal-entries")
@@ -26,17 +26,11 @@ public class JournalEntryController {
     }
 
     @GetMapping("/{requestedId}")
-    public ResponseEntity<String> findById(@PathVariable Long requestedId) throws JsonProcessingException {
+    public ResponseEntity<JournalEntry> findById(@PathVariable Long requestedId) throws JsonProcessingException {
+        Optional<JournalEntry> journalEntry = journalEntryRepository.findById(requestedId);
 
-
-        if (requestedId.equals(14L)) {
-            Long instantLong = Instant.now().toEpochMilli();
-            JournalEntry journalEntry = new JournalEntry(14L, "Deposit", instantLong, "-100", "500");
-            JavaTimeModule module = new JavaTimeModule();
-            objectMapper.registerModule(module);
-
-            String responseBody = new ObjectMapper().writeValueAsString(journalEntry);
-            return ResponseEntity.ok(responseBody);
+        if (journalEntry.isPresent()) {
+            return ResponseEntity.ok(journalEntry.get());
         } else {
             return ResponseEntity.notFound().build();
         }
