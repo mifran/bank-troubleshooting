38e87dd9298e73564d42b532794a813f0eee3d13
Mitch Franklin <biz@kingsforum.com>
2023-12-02 12:00:18 -0500

    GEN-21: change Account and JournalEntry to class from record

--- src/main/java/com/genecab/bank/Account.java
+++ src/main/java/com/genecab/bank/Account.java
@@ -1,7 +1,52 @@
 package com.genecab.bank;
 
+import jakarta.persistence.Entity;
 import org.springframework.data.annotation.Id;
 
-public record Account(@Id Long id, String name, String type, String owner) {
+@Entity
+public class Account {
 
+    @Id
+    private Long id;
+    private String name;
+    private String type;
+    private String owner;
+
+    // JPA requires a no argument constructor
+    protected Account() {}
+
+    public Account(Long id, String name, String type, String owner) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/genecab/bank/JournalEntry.java
+++ src/main/java/com/genecab/bank/JournalEntry.java
@@ -1,7 +1,73 @@
 package com.genecab.bank;
 
+import jakarta.persistence.Entity;
 import org.springframework.data.annotation.Id;
 
-public record JournalEntry(@Id Long id, Long account, String description, Long instant, String amount, String balance) {
+@Entity
+public class JournalEntry {
+    // @Id Long id, Long account, String description, Long instant, String amount, String balance
 
+    @Id
+    private Long id;
+    private Long account;
+    private String description;
+    private Long instant;
+    private String amount;
+    private String balance;
+
+    public JournalEntry() {
+    }
+
+    public JournalEntry(Long id, Long account, String description, Long instant, String amount, String balance) {
+        this.id = id;
+        this.account = account;
+        this.description = description;
+        this.instant = instant;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getAccount() {
+        return account;
+    }
+
+    public void setAccount(Long account) {
+        this.account = account;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getInstant() {
+        return instant;
+    }
+
+    public void setInstant(Long instant) {
+        this.instant = instant;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
 }
