a24f6c93371b41fd70142ae01ba93a942995042f
Mitch Franklin <biz@kingsforum.com>
2023-12-02 05:29:55 -0500

    GEN-20: Enable project to run and be accessible via Insomnia locally

--- src/main/java/com/genecab/bank/AccountRepository.java
+++ src/main/java/com/genecab/bank/AccountRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account, Long>, PagingAndSortingRepository<Account, Long> {
     Account findByIdAndOwner(Long id, String owner);
     Page<Account> findByOwner(String owner, PageRequest amount);
