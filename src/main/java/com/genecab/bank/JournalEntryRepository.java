d509192882dc040af6f3a0439669c06bc36e03ed
Mitch Franklin <biz@kingsforum.com>
2023-12-03 03:29:36 -0500

    GEN-21: organize imports; fix some red in IntelliJ

--- src/main/java/com/genecab/bank/JournalEntryRepository.java
+++ src/main/java/com/genecab/bank/JournalEntryRepository.java
@@ -6,14 +6,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface JournalEntryRepository extends CrudRepository<JournalEntry, Long>, PagingAndSortingRepository<JournalEntry, Long> {
 
     @Query(nativeQuery = true,
-           value = "SELECT balance FROM journal_entry je where id= (SELECT max(id) FROM journal_entry WHERE account = :account)")
+           value = "SELECT balance FROM JOURNAL_ENTRY je where id= (SELECT max(id) FROM JOURNAL_ENTRY WHERE account = :account)")
     List<String> getBalanceList(@Param("account") Long account);
 
 }
